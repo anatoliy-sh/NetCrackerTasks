3296868ea984631cde06b1947eed0fc1b6a0e095
anatoliy-sh <an_sheb@mail.ru>
2015-12-22 00:20:50 +0300

    Исправил: добавил try with resources.

--- Task4-JDBC/src/main/java/jdbc/Statements.java
+++ Task4-JDBC/src/main/java/jdbc/Statements.java
@@ -1,51 +1,60 @@
 package jdbc;
+
 import org.apache.log4j.Logger;
+
 import java.sql.*;
 
 /**
  * Created by Анатолий on 11.12.2015.
  */
-public  class Statements {
+public class Statements {
     private static final Logger log = Logger.getLogger(Statements.class);
-    public  void viewSubjects (Connection con) throws SQLException {
-        Statement stmt = con.createStatement();
-        String query = "SELECT * FROM subjects";
-        ResultSet rs = stmt.executeQuery(query);
-        log.info("SimpleStatement");
-        if(rs!=null)
-            while (rs.next()){
-                log.info("id: "+rs.getInt(1)+", name: " + rs.getString(2));
-            }
-        stmt.close();
-        rs.close();
-    }
-    //смписок предметов группы в конкретный день
-    public void prepareStatement (Connection con, int group, int day) throws SQLException{
-        PreparedStatement pstmt = con.prepareStatement("SELECT l.ID, s.name  FROM list l JOIN subjects s ON (l.subjID = s.ID) " +
-                "WHERE l.groupID = ? and l.day = ?");
-        pstmt.setInt(1,group);
-        pstmt.setInt(2,day);
-        ResultSet rs = pstmt.executeQuery();
-        log.info("PrepareStatement");
-        if(rs!=null)
-            while (rs.next()){
-                log.info("id: "+rs.getInt(1)+", name: " + rs.getString(2));
-            }
-        pstmt.close();
-        rs.close();
-    }
-    //количество пар
-    public void callableStatement (Connection con, int group, int day) throws SQLException{
-        CallableStatement cstm = con.prepareCall("{CALL para_len2(? , ? , ?) }");
-        //callableStatement.registerOutParameter(1, Types.INTEGER);
-        //cstm.setString(1,"para_len()");
-        cstm.setInt(1,group);
-        cstm.setInt(2,day);
-        cstm.registerOutParameter("MY_COUNT",Types.INTEGER);
-        cstm.execute();
-        log.info("CallableStatement");
-        log.info("Number of subjects (day - "+day+" group - "+group+")- "+cstm.getInt("MY_COUNT"));
 
-        cstm.close();
+    public void viewSubjects(Connection con) throws SQLException {
+        String query = "SELECT * FROM subjects";
+        try (Statement stmt = con.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
+            log.info("SimpleStatement");
+            if (rs != null)
+                while (rs.next()) {
+                    log.info("id: " + rs.getInt(1) + ", name: " + rs.getString(2));
+                }
+        } catch (SQLException e) {
+            log.error(e.getErrorCode() + e.getMessage(), e);
+        }
+    }
+
+    //смписок предметов группы в конкретный день
+    public void prepareStatement(Connection con, int group, int day) throws SQLException {
+        String query = "SELECT l.ID, s.name  FROM list l JOIN subjects s ON (l.subjID = s.ID) " +
+                "WHERE l.groupID = ? and l.day = ?";
+        try (PreparedStatement pstmt = con.prepareStatement(query)) {
+            pstmt.setInt(1, group);
+            pstmt.setInt(2, day);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                log.info("PrepareStatement");
+                if (rs != null)
+                    while (rs.next()) {
+                        log.info("id: " + rs.getInt(1) + ", name: " + rs.getString(2));
+                    }
+            } catch (SQLException e) {
+                log.error(e.getErrorCode() + e.getMessage(), e);
+            }
+        } catch (SQLException e) {
+            log.error(e.getErrorCode() + e.getMessage(), e);
+        }
+    }
+
+    //количество пар
+    public void callableStatement(Connection con, int group, int day) throws SQLException {
+        try (CallableStatement cstm = con.prepareCall("{CALL para_len2(? , ? , ?) }")) {
+            cstm.setInt(1, group);
+            cstm.setInt(2, day);
+            cstm.registerOutParameter("MY_COUNT", Types.INTEGER);
+            cstm.execute();
+            log.info("CallableStatement");
+            log.info("Number of subjects (day - " + day + " group - " + group + ")- " + cstm.getInt("MY_COUNT"));
+        } catch (SQLException e) {
+            log.error(e.getErrorCode() + e.getMessage(), e);
+        }
     }
 }
--- Task4-JDBC/src/main/java/jdbc/Test.java
+++ Task4-JDBC/src/main/java/jdbc/Test.java
@@ -10,32 +10,21 @@ import java.sql.*;
 
 public class Test {
     private static final Logger log = Logger.getLogger(Test.class);
-    public static void main(String[] args)  {
-        Connection con= null;
+
+    public static void main(String[] args) {
         MyConnection myCon = new MyConnection();
         Statements st = new Statements();
-        try {
-            con = myCon.getConnection();
-            if (con != null){
+        try (Connection con = myCon.getConnection()) {
+            if (con != null) {
                 st.viewSubjects(con);
-                st.prepareStatement(con,1,2);
-                st.callableStatement(con,1,2);
-            }
-            else
+                st.prepareStatement(con, 1, 2);
+                st.callableStatement(con, 1, 2);
+            } else
                 System.out.println(" unable to create connection");
-        }catch (SQLException e) {
-            log.error(e+e.getMessage()+e.getErrorCode(),e);
-        }
-        catch (Exception e) {
-            log.error(e+e.getMessage(),e);
-
-        }
-        finally {
-            if(con!=null) try {
-                con.close();
-            } catch (SQLException e) {
-                log.error(e+ e.getMessage()+e.getErrorCode(),e);
-            }
+        } catch (SQLException e) {
+            log.error(e.getMessage() + e.getErrorCode(), e);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
         }
     }
 }
